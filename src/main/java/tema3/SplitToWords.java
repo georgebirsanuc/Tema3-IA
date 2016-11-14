627d99c6053b3b42f7428ff53338c2fa22d60132
Birsanuc George-Cristian <Birsanuc George-Cristian>
2016-11-14 22:20:26 +0200

    xml output containing words

--- src/main/java/tema3/SplitToWords.java
+++ src/main/java/tema3/SplitToWords.java
@@ -2,7 +2,10 @@ package tema3;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -25,59 +28,81 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
-/** A simple corenlp example ripped directly from the Stanford CoreNLP website using text from wikinews. */
+/**
+ * A simple corenlp example ripped directly from the Stanford CoreNLP website
+ * using text from wikinews.
+ */
 public class SplitToWords {
 
-  public static void main(String[] args) throws IOException {
-    // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution 
-    Properties props = new Properties();
-    props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
-    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-    
-    // read some text from the file..
-    File inputFile = new File("src/main/resources/sample-content.txt");
-    String text = Files.toString(inputFile, Charset.forName("UTF-8"));
+	public static void main(String[] args) throws IOException {
+		String xml = "<words>\n";
+		String[] punctuation = {".", ",", "!", "?", ":", ";", "\\", "/", "\"", "(", ")", "[", "]", 
+				"{", "}", "-", "+", "=", "_", "#", "$", "%", "^", "&", "*", "~"};
 
-    // create an empty Annotation just with the given text
-    Annotation document = new Annotation(text);
+		// creates a StanfordCoreNLP object, with POS tagging, lemmatization,
+		// NER, parsing, and coreference resolution
+		Properties props = new Properties();
+		props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
-    // run all Annotators on this text
-    pipeline.annotate(document);
+		// read some text from the file..
+		File inputFile = new File("src/main/resources/sample-content.txt");
+		String text = Files.toString(inputFile, Charset.forName("UTF-8"));
 
-    // these are all the sentences in this document
-    // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-    List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+		text.replaceAll("[.,\\/#!$%^&*;:{}=-_+~()]", "");
+		System.out.println("NO PUNCT: " + text);
+		// create an empty Annotation just with the given text
+		Annotation document = new Annotation(text);
 
-    for(CoreMap sentence: sentences) {
-      // traversing the words in the current sentence
-      // a CoreLabel is a CoreMap with additional token-specific methods
-      for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
-        // this is the text of the token
-        String word = token.get(TextAnnotation.class);
-        // this is the POS tag of the token
-        String pos = token.get(PartOfSpeechAnnotation.class);
-        // this is the NER label of the token
-        String ne = token.get(NamedEntityTagAnnotation.class);
-        
-        System.out.println("word: " + word + " pos: " + pos + " ne:" + ne);
-      }
+		// run all Annotators on this text
+		pipeline.annotate(document);
 
-      // this is the parse tree of the current sentence
-      Tree tree = sentence.get(TreeAnnotation.class);
-      System.out.println("parse tree:\n" + tree);
+		// these are all the sentences in this document
+		// a CoreMap is essentially a Map that uses class objects as keys and
+		// has values with custom types
+		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
 
-      // this is the Stanford dependency graph of the current sentence
-      SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
-      System.out.println("dependency graph:\n" + dependencies);
-    }
+		for (CoreMap sentence : sentences) {
+			// traversing the words in the current sentence
+			// a CoreLabel is a CoreMap with additional token-specific methods
+			for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+				// this is the text of the token
+				String word = token.get(TextAnnotation.class);
+				// this is the POS tag of the token
+				String pos = token.get(PartOfSpeechAnnotation.class);
+				// this is the NER label of the token
+				String ne = token.get(NamedEntityTagAnnotation.class);
 
-    // This is the coreference link graph
-    // Each chain stores a set of mentions that link to each other,
-    // along with a method for getting the most representative mention
-    // Both sentence and token offsets start at 1!
-    Map<Integer, CorefChain> graph = 
-        document.get(CorefChainAnnotation.class);
-    
-  }
+				if (!Arrays.asList(punctuation).contains(word))
+					xml += "\t<word> " + word + " </word>\n";
+				System.out.println("word: " + word + " pos: " + pos + " ne:" + ne);
+			}
+
+			// this is the parse tree of the current sentence
+			Tree tree = sentence.get(TreeAnnotation.class);
+			System.out.println("parse tree:\n" + tree);
+
+			// this is the Stanford dependency graph of the current sentence
+			SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
+			System.out.println("dependency graph:\n" + dependencies);
+		}
+
+		xml += "</words>\n";
+		System.out.println(xml);
+		PrintWriter writer = new PrintWriter("src/main/resources/nlp.xml", "UTF-8");
+		writer.print(xml);
+		writer.close();
+		
+//		nlp.xml output file (nu chiar ce vrem)
+//		FileOutputStream os = new FileOutputStream(new File("src/main/resources/", "nlp.xml"));
+//	    pipeline.xmlPrint(document, os);
+	    
+		// This is the coreference link graph
+		// Each chain stores a set of mentions that link to each other,
+		// along with a method for getting the most representative mention
+		// Both sentence and token offsets start at 1!
+		Map<Integer, CorefChain> graph = document.get(CorefChainAnnotation.class);
+
+	}
 
 }
